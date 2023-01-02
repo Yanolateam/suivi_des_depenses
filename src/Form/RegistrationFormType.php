8f722e2d591fdeeabcced0a6a5444af7c4434c52
Yanolateam <yanolabteam@gmail.com>
2023-01-02 16:08:52 +0100

    v 0.1

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,10 +22,10 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('status', ChoiceType::class, [
+                'attr' => ['class' => 'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'],
                 'choices'  => [
                     'Particulier' => 'Particulier',
-                    'Professionnel' => 'Professionnel'
-                    
+                    'Professionnel' => 'Professionnel'  
                 ],
             ])
             ->add('birthday', DateType::class,[
