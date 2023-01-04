c555ffa58d93f67ce351816eb6b4fccd057a08f7
Yanolateam <yanolabteam@gmail.com>
2023-01-04 14:31:03 +0100

    v 0.1

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -30,7 +30,12 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('birthday', DateType::class,[
                 'attr' => ['class' => 'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'],
-
+                'years' => range(1920,2100),
+                'placeholder' => [
+                    'year' => 'Année', 'month' => 'Mois', 'day' => 'Jour',
+                ],
+                'choice_translation_domain' => true
+                
             ])
             ->add('firstname', TextType::class,[
                 'attr' => ['class' => 'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'],
@@ -78,6 +83,7 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'translation_domain' => 'forms'
         ]);
     }
 }
