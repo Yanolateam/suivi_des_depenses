37064dd575ff82a60d8f6674f4be3783cd5d6398
Yanolateam <yanolabteam@gmail.com>
2023-01-06 11:29:02 +0100

    v 0.1

--- src/Form/DepenseFormType.php
+++ src/Form/DepenseFormType.php
@@ -12,13 +12,21 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 
 class DepenseFormType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        
+        $user = $this->security->getUser();
 
         $builder
             
@@ -56,6 +64,8 @@ class DepenseFormType extends AbstractType
                     ],
                 ],
             ])
+
+            
 
             ->add('fournisseur', EntityType::class, options:[
                 'attr' => ['class' => 'relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'],
